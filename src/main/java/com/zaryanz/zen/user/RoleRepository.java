ac2f8b291379fa29525480aa92a8b6e9796ab9c9
zaryanz <aryanx404@gmail.com>
2024-02-25 11:32:43 +0530

    feat: introduced jpa repositories for user entity and role

---
+++ src/main/java/com/zaryanz/zen/user/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.zaryanz.zen.user;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+	Optional<Role> findByName(String name);
+}
---
+++ src/main/java/com/zaryanz/zen/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.zaryanz.zen.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+	Optional<UserEntity> findByUsername(String username);
+	Boolean existsByUsername(String username);
+}
